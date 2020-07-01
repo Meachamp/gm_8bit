8a9e1852074acdfdc8d8350c48079e383d4f7419
Meachamp <34523297+Meachamp@users.noreply.github.com>
2020-07-01 04:26:42 -0400

    Allocate a codec instance per player using affected

--- source/main.cpp
+++ source/main.cpp
@@ -7,7 +7,7 @@
 #include <eifacev21.h>
 #include <iclient.h>
 #include <ivoicecodec.h>
-#include <unordered_set>
+#include <unordered_map>
 
 #if defined SYSTEM_WINDOWS
 	#define WIN32_LEAN_AND_MEAN
@@ -26,38 +26,34 @@
 #endif
 
 static int crushFactor = 8;
-static IVoiceCodec* g_pVoiceCodec = nullptr;
 static short decompressedBuffer[11500];
 static char recompressBuffer[11500*2];
 static IVEngineServer* engine_ptr = nullptr;
+CreateInterfaceFn speexFactory = nullptr;
+static bool didInit = false;
 
 typedef void (*SV_BroadcastVoiceData)(IClient* cl, int nBytes, char* data, int64 xuid);
 MologieDetours::Detour<SV_BroadcastVoiceData>* detour_BroadcastVoiceData = nullptr;
 
-std::unordered_set<int> afflicted_players;
-
-
+std::unordered_map<int, IVoiceCodec*> afflicted_players;
 
 void hook_BroadcastVoiceData(IClient* cl, int nBytes, char* data, int64 xuid) {
 	//Check if the player is in the set of enabled players.
 	//This is (and needs to be) and O(1) operation for how often this function is called. 
 	//If not in the set, just hit the trampoline to ensure default behavior. 
-	std::cout << "RUNNING VOICE0" << std::endl;
-	std::cout << cl->GetUserID() << std::endl;
-	if (afflicted_players.find(cl->GetUserID()) != afflicted_players.end()) {
+	int uid = cl->GetUserID();
+	if (afflicted_players.find(uid) != afflicted_players.end()) {
 		//Decompress the stream with vaudio_speex
 		//Produces signed 16-bit PCM samples @ 11025 Hz
 		//Output is # samples produced.
-		int samples = g_pVoiceCodec->Decompress(data, nBytes, (char*)decompressedBuffer, sizeof(decompressedBuffer));
-		std::cout << "RUNNING VOICE1" << std::endl;
+		IVoiceCodec* codec = afflicted_players.at(uid);
+		int samples = codec->Decompress(data, nBytes, (char*)decompressedBuffer, sizeof(decompressedBuffer));
 		//Speex will return a negative (or zeroed) number if decompression fails. 
 		if (samples <= 0) {
 			//Just hit the trampoline at this point.
 			std::cout << "Decompression failed: " << samples << std::endl;
 			return detour_BroadcastVoiceData->GetOriginalFunction()(cl, nBytes, data, xuid);
 		}
-
-		std::cout << "RUNNING VOICE" << std::endl;
 
 		//Bit crush the stream
 		for (int i = 0; i < samples; i++) {
@@ -72,7 +68,7 @@ void hook_BroadcastVoiceData(IClient* cl, int nBytes, char* data, int64 xuid) {
 		}
 
 		//Recompress the stream
-		int bytesWritten = g_pVoiceCodec->Compress((char*)decompressedBuffer, samples, recompressBuffer, sizeof(recompressBuffer), true);
+		int bytesWritten = codec->Compress((char*)decompressedBuffer, samples, recompressBuffer, sizeof(recompressBuffer), true);
 
 		//Broadcast voice data with our update compressed data.
 		return detour_BroadcastVoiceData->GetOriginalFunction()(cl, bytesWritten, recompressBuffer, xuid);
@@ -99,10 +95,19 @@ LUA_FUNCTION_STATIC(zsutil_enable8bit) {
 	int id = LUA->GetNumber(1);
 	bool b = LUA->GetBool(2);
 
-	if (b) {
-		afflicted_players.insert(id);
+	if (!didInit) {
+		LUA->ThrowError("Module did not successfully init!");
+		return 0;
+	}
+
+	if (b && afflicted_players.find(id) == afflicted_players.end()) {
+		IVoiceCodec* codec = (IVoiceCodec*)speexFactory("vaudio_speex", nullptr);
+		codec->Init(4);
+		afflicted_players.insert(std::pair<int, IVoiceCodec*>(id, codec));
 	}
 	else {
+		IVoiceCodec* codec = afflicted_players.at(id);
+		codec->Release();
 		afflicted_players.erase(id);
 	}
 
@@ -141,13 +146,6 @@ GMOD_MODULE_OPEN()
 		return 0;
 	}
 
-	try {
-		detour_BroadcastVoiceData = new MologieDetours::Detour<SV_BroadcastVoiceData>((SV_BroadcastVoiceData)sv_bcast, hook_BroadcastVoiceData);
-	} catch (...) {
-		std::cout << "SV_BroadcastVoiceData Detour failed!" << std::endl;
-		return 0;
-	}
-
 	LoadSpeex();
 	
 	SourceSDK::FactoryLoader speex("vaudio_speex");
@@ -156,23 +154,43 @@ GMOD_MODULE_OPEN()
 		return 0;
 	}
 
-	IVoiceCodec* codec = speex.GetInterface<IVoiceCodec>("vaudio_speex");
-	bool res = codec->Init(4);
-	std::cout << "INIT STATUS: " << res << std::endl;
-	g_pVoiceCodec = codec;
+	speexFactory = speex.GetFactory();
 
-	afflicted_players = std::unordered_set<int>();
+	if (speexFactory == nullptr) {
+		std::cout << "Could not access Speex Factory!" << std::endl;
+		return 0;
+	}
+
+	try {
+		detour_BroadcastVoiceData = new MologieDetours::Detour<SV_BroadcastVoiceData>((SV_BroadcastVoiceData)sv_bcast, hook_BroadcastVoiceData);
+	}
+	catch (...) {
+		std::cout << "SV_BroadcastVoiceData Detour failed!" << std::endl;
+		return 0;
+	}
+
+	afflicted_players = std::unordered_map<int, IVoiceCodec*>();
 
 	//If we want to use the speex codec, we need to disable steam voice to force it to be used by clients.
 	engine_ptr->GMOD_RawServerCommand("sv_use_steam_voice 0");
+	didInit = true;
 	return 0;
 }
 
 GMOD_MODULE_CLOSE()
 {
 	delete detour_BroadcastVoiceData;
-	g_pVoiceCodec->Release();
-	g_pVoiceCodec = nullptr;
+	speexFactory = nullptr;
 	engine_ptr = nullptr;
+	didInit = false;
+
+	for (auto& p : afflicted_players) {
+		if (p.second != nullptr) {
+			p.second->Release();
+		}
+	}
+
+	afflicted_players.clear();
+
 	return 0;
 }
