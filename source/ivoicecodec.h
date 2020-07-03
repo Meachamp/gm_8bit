c041e0cae56b9d5ccccf02cc5bbb36454a1c7cfa
Meachamp <34523297+Meachamp@users.noreply.github.com>
2020-07-03 00:06:40 -0400

    Update IVoiceCodec header

--- source/ivoicecodec.h
+++ source/ivoicecodec.h
@@ -4,7 +4,8 @@ class IVoiceCodec
 protected:
 	virtual			~IVoiceCodec() {}
 public:
-	virtual bool	Init(int quality) = 0;
+	virtual bool	Init(int quality, int sampleRate) = 0;
+	virtual int		GetSampleRate() = 0;
 	virtual void	Release() = 0;
 	virtual int		Compress(const char *pUncompressed, int nSamples, char *pCompressed, int maxCompressedBytes, bool bFinal) = 0;
 	virtual int		Decompress(const char *pCompressed, int compressedBytes, char *pUncompressed, int maxUncompressedBytes) = 0;
