e00d13f458e1f7ed7e08d223ea39db53245ec967
Meachamp <34523297+Meachamp@users.noreply.github.com>
2020-07-07 00:34:33 -0400

    Untest linux socket implementation

--- source/net.cpp
+++ source/net.cpp
@@ -5,6 +5,14 @@
 #include <WS2tcpip.h>
 #endif
 
+#ifdef __linux
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <unistd.h>
+#endif
+
 Net::Net() {
 #ifdef _WIN32
 	WSADATA wsaData;
@@ -16,20 +24,18 @@ Net::Net() {
 	if (m_socket == INVALID_SOCKET)
 		throw "Invalid socket!";
 #elif __linux
-
+	m_socket = socket(AF_INET, SOCK_DGRAM, 0);
+	if (m_socket < 0)
+		throw "Invalid socket!";
 #endif
 }
 
 void Net::SendPacket(const char* dest, const char* buffer, uint32_t len) {
-#ifdef _WIN32
 	sockaddr_in dest_str;
 	dest_str.sin_family = AF_INET;
 	dest_str.sin_port = htons(4000);
 	inet_pton(AF_INET, dest, &dest_str.sin_addr);
 	sendto(m_socket, buffer, len, 0, (sockaddr*)&dest_str, sizeof(dest_str));
-#elif __linux
-
-#endif
 }
 
 Net::~Net() {
@@ -37,6 +43,6 @@ Net::~Net() {
 	closesocket(m_socket);
 	WSACleanup();
 #elif __linux
-
+	close(m_socket);
 #endif
 }
